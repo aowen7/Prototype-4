d3c35721273992e00a5dc7cb7f4d571c6a9975f5
aowen7 <122053762+aowen7@users.noreply.github.com>
2023-03-03 12:21:28 -0800

    initial commit
    enemy spawns randomly

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -12,12 +12,8 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        float spawnPosX = Random.Range(-spawnRange, spawnRange);
-        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
-        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         
-        
-        Instantiate(enemyPrefab, randomPos, enemyPrefab.transform.rotation);
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
@@ -25,4 +21,14 @@ public class SpawnManager : MonoBehaviour
     {
         
     }
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+            float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+            
+            Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+            return randomPos;
+    }
 }
+        
+
