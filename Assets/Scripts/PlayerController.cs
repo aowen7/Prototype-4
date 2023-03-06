12103274a0d7cadf96523c05ae87866d08d6996f
aowen7 <122053762+aowen7@users.noreply.github.com>
2023-03-06 11:43:45 -0800

    initial commit
    knock back strength

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -35,11 +35,17 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private float powerupStrength = 15.0f;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
         {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
             Debug.Log("Collided with " + collision.gameObject.name + " With powerup set to " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
         }
     }
 }
