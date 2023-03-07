ef018b9cf11ee84a3978b89c9d5f86fe79e9d39e
aowen7 <122053762+aowen7@users.noreply.github.com>
2023-03-07 10:34:07 -0800

    initial commit
    added powerup indicator

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    private float powerupStrength = 15.0f;
     public float speed = 5.0f;
     public bool hasPowerup;
-    
+    public GameObject powerupIndicator;
     
 
     // Start is called before the first frame update
@@ -24,6 +25,8 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -31,6 +34,7 @@ public class PlayerController : MonoBehaviour
         if(other.CompareTag("Powerup"))
         {
             hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
             StartCoroutine(PowerupCountdownRoutine());
         }
@@ -40,9 +44,9 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(7);
         hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
     }
 
-    private float powerupStrength = 15.0f;
 
     private void OnCollisionEnter(Collision collision)
     {
